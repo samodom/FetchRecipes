9c2050703b19007c1a343240dfcd314e896c4597
Sam Odom <samodom@icloud.com>
2025-03-03 16:41:40 -0700

    Add Main dependency bundle for top-level of app

---
+++ FetchRecipes/Main.swift
@@ -0,0 +1,60 @@
+import AppleNetworkingImplementation
+import CachingInterface
+import CachingImplementations
+import Foundation
+import Model
+import NetworkingInterface
+import Providers
+import SwiftData
+
+/// A top-level bundle of dependencies use in this application. It is owned by the `App` instance.
+final class Main {
+    let fetcher: any RemoteResourceFetching
+    let modelContainer: ModelContainer
+    let recipeProvider: any RecipeProvider
+    let imageProvider: any ImageProvider
+    let recipesURL = URL(string: "https://d3jbb8n5wk0qxi.cloudfront.net/recipes.json")!
+
+    init() {
+        // Using a custom session with this configuration is an attempt to keep the cache from
+        // failing to clear all items. Images load immediately despite attempting to clear them.
+        let urlSessionConfiguration = URLSessionConfiguration.ephemeral
+        urlSessionConfiguration.requestCachePolicy = .reloadIgnoringLocalAndRemoteCacheData
+        urlSessionConfiguration.urlCache?.diskCapacity = 0
+        urlSessionConfiguration.urlCache?.memoryCapacity = 0
+        let urlSession = URLSession(configuration: urlSessionConfiguration)
+        let fetcher = AppleRemoteResourceFetcher(networker: urlSession)
+
+        do {
+            modelContainer = try ModelContainer(
+                for: Recipe.self,
+                configurations: ModelConfiguration(for: Recipe.self, isStoredInMemoryOnly: true)
+            )
+        }
+        catch {
+            print(error.localizedDescription)
+            fatalError("Unable to launch app")
+        }
+
+        recipeProvider = DefaultRecipeProvider(
+            fetcher: fetcher,
+            modelContainer: modelContainer
+        )
+
+        let cache = DefaultRemoteDataCache(urlCache: Self.createURLCache())
+        imageProvider = DefaultImageProvider(
+            fetcher: fetcher,
+            cache: cache
+        )
+
+        self.fetcher = fetcher
+    }
+
+    private static func createURLCache() -> URLCache {
+        let directory = FileManager.default.temporaryDirectory.standardizedFileURL
+            .appending(path: "images")
+        let megabytes = Measurement(value: 10, unit: UnitInformationStorage.megabytes)
+        let bytes = Int(megabytes.converted(to: .bytes).value)
+        return URLCache(memoryCapacity: bytes, diskCapacity: bytes, directory: directory)
+    }
+}
